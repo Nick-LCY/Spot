7d565e197c4237d4c96b5d480e6c2d8ad258e05f
NickLin <nicklin9907@gmail.com>
2024-11-22 02:47:49 +0000

    Miss file

--- configs.py
+++ configs.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass
 from instance_arch_and_ami_id import IMAGE_ID, FAMILY_ARCH
 
 # Top K instances that need to collect launch time
-TOP_K = 3
+LAUNCH_TOP_K = 3
 # How many spots should be launched
 INSTANCE_COUNT = 10
 
@@ -10,6 +10,9 @@ INSTANCE_COUNT = 10
 # This value shouldn't be too large, otherwise, you may violate
 # [SPS Limitations](https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/spot-placement-score.html#sps-limitations)
 SAMPLE_COUNT = 20
+# Top K instances that need to select from yeseterday data
+SPS_AND_IF_TOP_K = 10
+# Notes, total number of instance that will collect SPS and IF = SAMPLE_COUNT + SPS_AND_IF_TOP_K
 
 # Timeout if instances not ready after 6 minutes when collecting launch time
 TIMEOUT = 60 * 6
